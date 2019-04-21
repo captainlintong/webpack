40e68e0af8bfeb4e39fe03fb0ba84debe5941958
captainlintong <good_pengyou@sina.com>
2019-04-22 02:37:11 +0800

    es6转es5及babel缓存

--- src/index.js
+++ src/index.js
@@ -15,3 +15,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import './styles/main.less'
 
 foo()
+const msg = '你好啊'
+/**
+ * babel默认智能转换核心语法 例如 const  let 箭头函数、类.....
+ * 无法转换新增的函数，例如数组的includes， 字符串的startsWith， endsWith
+ *
+ */
--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,17 @@ module.exports = {
           'css-loader', // css-loader的作用是将css文件转换为一个js模块
           'less-loader', // 将less转换为css
         ]
+      },
+      {
+        test: /\.m?js$/, // 当加载以.js 或 .mjs文件的时候，使用babel-loader将代码中的ES6转换为ES5
+        exclude: /(node_modules|bower_components)/, // 排出这些文件中的模块不转换，只转换我们自己的ES6代码，因为一般第三方包提供的 都是转换之后的
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], // 转换规则
+            cacheDirectory: true // babel转换比较耗时，这里是开启结果缓存，它会将每次缓存的结果缓存到node_modules/.cache目录中 ，下次打包如果文件没有变，会直接把缓存拿出来，提高转换效率
+          }
+        }
       }
     ]
   }
