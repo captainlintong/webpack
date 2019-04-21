c5bd492c5f3d06d3247249d56b9567ce691ca142
captainlintong <good_pengyou@sina.com>
2019-04-22 00:58:11 +0800

    生产模式和开发模式打包的介绍

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,11 @@ module.exports = {
   output: {
     path: path.join(__dirname, './dist'),  // 将打包结果放到 dist 目录中
     filename: 'main.js' // 自定打包结果的文件名
-  }
+  },
+  /*
+ production 生产模式打包 速度慢 有优化 （会压缩代码） 发布上线用
+ development 开发模式构建 速度快 没有什么优化
+ 默认是production
+ */
+  mode: 'development'
 }
